e7bb8041fc0bb289781b5407916cefc261f357ba
Y Li <oxfrombws@gmail.com>
2018-04-21 16:51:07 -0400

    Clean up brother cupwrapper install script

--- drivers/install_brother_lpr_driver.sh
+++ drivers/install_brother_lpr_driver.sh
@@ -4,10 +4,12 @@ set -e
 
 # See http://support.brother.com/g/s/id/linux/en/instruction_prn1a.html
 # install libstdc++6 reuiqred by LPR and CPU wrapper
-dpkg --add-architecture i386 && \
-apt-get update && apt-get install -y libstdc++6:i386 || exit 1
 
-find . -type f -name '*cupswrapper*.deb' -printf '%f\n' | sort | \
+tmpfile=$(mktemp brother_lpr_found.XXXXXX)
+trap "rm -f ${tmpfile}" INT TERM HUP EXIT
+find . -type f -name '*cupswrapper*.deb' -printf '%f\n' | sort > ${tmpfile}
+
+DRV_PKG=
 while read -r cupswrapper ; do
   model=$(echo $cupswrapper|sed 's;cupswrapper.*;;')
   lpr=$(find . -type f -name "${model}lpr*.deb" -printf '%f\n')
@@ -15,8 +17,13 @@ while read -r cupswrapper ; do
     echo "Cann't find LPR driver for ${model} cupswrapper driver"
 	exit 1
   fi
-  echo "Installing cupswrapper LPR drivers for Brother ${model}"
-  dpkg -i ${lpr} && \
-  dpkg -i ${cupswrapper} || \
-  exit 1
-done
+  echo "Adding cupswrapper LPR drivers for Brother ${model}"
+  DRV_PKG=${DRV_PKG:+ ${lpr} ${cupswrapper}}
+  DRV_PKG=${DRV_PKG:-${lpr} ${cupswrapper}}
+done < ${tmpfile}
+
+if [ ! -z "${DRV_PKG}" ]; then
+  dpkg --add-architecture i386 && apt-get update && \
+    apt-get install -y libstdc++6:i386 && \
+	dpkg -i ${DRV_PKG} || exit 1
+fi
